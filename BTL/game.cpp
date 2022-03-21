f3b06e5e02a882485dc73766e5352b8a16ec987a
Le Ha <minhhatrang12@gmail.com>
2022-03-21 12:57:49 +0700

    commit

--- BTL/game.cpp
+++ BTL/game.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
+#include <ctime>
+#include "Piece.h"
 
 using namespace std;
 
-int a[10][10] = {0};
+int board[20][10] = {0};
+
+void getNewPiece(Piece &newPiece)
+{
+    int Shape = rand()%7;
+    int Rotation = rand()%4;
+    int X;
+    int Y;
+    newPiece.getTetromino(Shape, Rotation, X, Y);
+    newPiece.getBeginXPos(Shape, Rotation);
+    newPiece.getBeginYPos(Shape, Rotation);
+}
 
 bool GameOver();
 
-void GameInit();
+void GameInit()
+{
+}
 
-void UpdateGame(int newPiece);
+void UpdateGame(Piece newPiece);
 
 void PrintMap();
 
 int main()
 {
+    srand(time(0));
+
     while (!GameOver)
     {
+        Piece newPiece;
+        getNewPiece(newPiece);
         GameInit();
-        int newPiece;
+        
         UpdateGame(newPiece);
         PrintMap();
     }
