9524fcea3bd105f6adff8dcf801632bf76f01d5f
Le Ha <minhhatrang12@gmail.com>
2022-04-18 12:15:34 +0700

    commit

--- BT10/C1.cpp
+++ BT10/C1.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 struct String
 {
-    unsigned int n;
+    unsigned int n = 0;
     char* str;
     String(const char* text)
     {
-        unsigned int _n = 0;
-        for (*str = *text; (*str) != '\0'; str++) _n++;
-        n = _n;
+        for (char *p = text; (*p) != '\0'; p++) n++;
         str = new char[n];
+        for (int i = 0; i < n; i++)
+        {
+            str[i] = text[i];
+        }
     }
     void print()
     {
-        for (int i = 0; i<n; i++) cout << str[i];
+        for (int i = 0; i<n; i++) cout << *(str+i);
     }
 
     ~String()
